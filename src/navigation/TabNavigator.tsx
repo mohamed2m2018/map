f00a3c616065a13b37ec21cb8072608a583d82b0
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2022-07-17 13:48:45 +0200

    adding navigation

---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,108 @@
+import React, {useContext} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import Map from '../screens/Map';
+import AppIcon from '../components/AppIcon';
+import {ThemeContext} from '../context/ThemeContext';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  const {colors} = useContext(ThemeContext);
+  return (
+    <Tab.Navigator
+      initialRouteName="Map"
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: colors.tabBarBackground,
+        },
+      }}>
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <AppIcon
+              type="Feather"
+              color={colors.secondary}
+              name="compass"
+              size={25}
+            />
+          ),
+        }}
+        component={() => <Text>hello world</Text>}
+        name="Explore"
+      />
+      <Tab.Screen
+        component={Map}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <AppIcon
+              type="Ionicons"
+              color={colors.secondary}
+              name="map"
+              size={25}
+            />
+          ),
+        }}
+        name="Map"
+      />
+      <Tab.Screen
+        component={() => <Text>hello world</Text>}
+        options={({navigation}) => ({
+          tabBarButton: () => (
+            <TouchableOpacity
+              onPress={() => navigation?.navigate('Add')}
+              style={styles.middleButton}>
+              <AppIcon type="Entypo" color="white" name="plus" size={25} />
+            </TouchableOpacity>
+          ),
+        })}
+        name="Add"
+      />
+      <Tab.Screen
+        component={() => <Text>hello world</Text>}
+        name="Notifications"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <AppIcon
+              type="MaterialIcons"
+              color={colors.secondary}
+              name="notifications"
+              size={25}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        component={() => <Text>hello world</Text>}
+        name="Account"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <AppIcon
+              type="MaterialCommunityIcons"
+              color={colors.secondary}
+              name="account-outline"
+              size={25}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  middleButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#FE3139',
+    bottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
