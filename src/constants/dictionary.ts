964330c7e9a4a4791abdcafbe8eea44000b6e17a
Mohamed Mohamed Salah <mohamed2m2018@gmail.com>
2022-07-17 13:47:06 +0200

    adding dictionary

---
+++ src/constants/dictionary.ts
@@ -0,0 +1,7 @@
+const dictionary = {
+  somethingWrong: 'Something wrong happened',
+  choosePlace: 'Please choose a place first',
+  noAccess: "Can't access location",
+};
+
+export default dictionary;
